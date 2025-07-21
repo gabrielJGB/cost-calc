1e809a3d1aca783d38f24942869e75401a005eba
gabriel <brunengo.gabriel@gmail.com>
2025-07-21 01:24:49 -0300

    update

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,8 @@ export default function PriceCalculator() {
   const [vatRate, setVatRate] = useState(21);
   const [marginRate, setMarginRate] = useState(50);
   const [quantity, setQuantity] = useState(1);
+  const [item,setItem] = useState("");
+  const darkMode = true
 
   // Cálculo de costo sin IVA según inclusión
   const costSinIVA = useMemo(() => {
@@ -41,23 +43,31 @@ export default function PriceCalculator() {
   return (
     <div className="p-2  md:p-4 mx-auto flex flex-col justify-center md:flex-row md:gap-4 gap-4 mb-40">
       {/* Sección Costo */}
-      <div className="bg-green-100 rounded-xl px-4 py-2 shadow ">
+      <div className="bg-[#0c4b2d] rounded-xl px-4 py-2 shadow shadow-gray-900 ">
         <h2 className="text-lg font-bold mb-2">COSTO</h2>
         <div className="grid grid-cols-2 gap-3">
+          <textarea className='col-span-2 text-xs p-2 bg-[#0f5e38] rounded'>
+
+          </textarea>
+          {/* <input
+            type="text"
+            value={item}
+            onInput={e => setItem(e.target.value)}
+            className="rounded p-1 text-xs col-span-2" 
+          /> */}
+
           <label className="text-sm md:text-md self-center">Precio Lista:</label>
           <input
             type="number"
             value={priceList}
             onInput={e => setPriceList(parseFloat(e.target.value) || 0)}
-            className="rounded p-1 bg-green-200"
-            // onBlur={e => setPriceList(unfmt(e.target.value))}
-            // onInput={e => setPriceList(unfmt(e.target.value))}
+            className="rounded p-1 bg-[#0f5e38]" 
           />
           <label className="text-sm md:text-md">Moneda:</label>
           <select
             value={currency}
             onChange={e => setCurrency(e.target.value)}
-            className="text-sm rounded p-1 bg-green-200"
+            className="text-sm rounded p-1 bg-[#0f5e38]"
           >
             <option value="USD">USD</option>
             <option value="ARS">ARS</option>
@@ -72,7 +82,7 @@ export default function PriceCalculator() {
                 type="number"
                 value={exchangeRate}
                 onInput={e => setExchangeRate(parseFloat(e.target.value) || 1)}
-                className=" p-1 bg-green-200 self-center"
+                className=" p-1 bg-[#0f5e38] self-center"
               />
             </>
           )}
@@ -89,7 +99,7 @@ export default function PriceCalculator() {
           <select
             value={vatRate}
             onChange={e => setVatRate(parseFloat(e.target.value))}
-            className=" rounded p-1 bg-green-200"
+            className=" rounded p-1 bg-[#0f5e38]"
           >
             <option value={10.5}>10.5%</option>
             <option value={21}>21%</option>
@@ -109,21 +119,24 @@ export default function PriceCalculator() {
           <label className="text-sm md:text-md">Precio sin IVA (ARS):</label>
           <div className="font-mono">{fmt(costSinIVAARS)}</div>
 
-          <label className="font-bold text-sm md:text-md">Costo final (ARS):</label>
+          <label className="font-bold text-sm md:text-md">Costo unidad (ARS):</label>
           <div className="font-mono font-bold">{fmt(costFinalARS)}</div>
+
+          <label className="font-bold text-sm md:text-md">Costo total (ARS):</label>
+          <div className="font-mono font-bold">{fmt(costFinalARS*quantity)}</div>
         </div>
       </div>
 
       {/* Sección Venta */}
-      <div className="bg-blue-100 rounded-xl px-4 py-2 shadow md:w-1/3">
-        <h2 className="text-lg font-bold mb-2">VENTA</h2>
+      <div className="bg-[#21387d] rounded-xl px-4 py-2 shadow md:w-1/3 shadow shadow-gray-900">
+        <h2 className="text-lg font-bold mb-2 ">VENTA</h2>
         <div className="grid grid-cols-2 gap-2">
           <label className="text-sm md:text-md self-center">Margen (%):</label>
           <input
             type="number"
             value={marginRate}
             onInput={e => setMarginRate(parseFloat(e.target.value) || 0)}
-            className="bg-blue-200 rounded p-1"
+            className="bg-[#2543ab] rounded p-1"
           />
 
           <label className="text-sm md:text-md">Margen (ARS):</label>
@@ -134,11 +147,11 @@ export default function PriceCalculator() {
             type="number"
             value={quantity}
             onInput={e => setQuantity(parseInt(e.target.value) || 1)}
-            className="bg-blue-200 rounded p-1"
+            className="bg-[#2543ab] rounded p-1"
           />
 
           <label className="text-sm md:text-md">Margen total (ARS):</label>
-          <div className="font-mono">{fmt(marginAmountARS*quantity)}</div>
+          <div className="font-mono">{fmt(marginAmountARS * quantity)}</div>
 
           <label className="text-sm md:text-md self-center font-bold ">Precio unidad (ARS):</label>
           <div className="font-mono font-bold ">{fmt(unitPriceARS)}</div>
